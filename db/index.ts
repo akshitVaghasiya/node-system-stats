0d3c1550853ccea49aa672856863f17371ea02cc
Akshit Vaghasiya <akshitvaghasiya2003@gmail.com>
2025-03-11 12:27:58 +0530

    change in db connection

--- db/index.ts
+++ db/index.ts
@@ -3,18 +3,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const pool = new Pool({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PASSWORD,
+//     port: Number(process.env.DB_PORT)
+// });
+
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT)
+    connectionString: process.env.DB_CONNECTION_STRING,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-pool.query("select 1", (err, res)=> {
-    if(err) {
+pool.query("select 1", (err, res) => {
+    if (err) {
         console.log("database connection error", err);
-    }else{
+    } else {
         console.log("database connected");
     }
 });
